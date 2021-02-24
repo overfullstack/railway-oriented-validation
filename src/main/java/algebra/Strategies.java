16686909f721e388f5e31a4b67184e17888c1614
Gopal S Akshintala <gopala.akshintala@salesforce.com>
2021-02-24 11:45:08 +0530

    Removed Throwable Validator

--- src/main/java/algebra/Strategies.java
+++ src/main/java/algebra/Strategies.java
@@ -1,16 +1,18 @@
 package algebra;
 
-import algebra.types.AccumulationStrategy;
-import algebra.types.FailFastStrategy;
 import algebra.types.Validator;
+import io.vavr.Function1;
 import io.vavr.collection.Iterator;
 import io.vavr.collection.List;
 import io.vavr.control.Either;
+import io.vavr.control.Try;
 import lombok.experimental.UtilityClass;
 import lombok.val;
 
 import java.util.ArrayList;
 import java.util.function.BiFunction;
+
+import static io.vavr.Function1.identity;
 
 /**
  * These Strategies compose multiple validations and return a single function which can be applied on the Validatable.
@@ -21,7 +23,9 @@ import java.util.function.BiFunction;
 public class Strategies {
 
     public static <FailureT, ValidatableT> java.util.List<Either<FailureT, ?>> runAllValidationsFailFastImperative(
-            List<ValidatableT> validatables, List<Validator<ValidatableT, FailureT>> validations, FailureT invalidValidatable) {
+            List<ValidatableT> validatables, List<Validator<ValidatableT, FailureT>> validations, 
+            FailureT invalidValidatable,
+            Function1<Throwable, FailureT> throwableMapper) {
         var validationResults = new ArrayList<Either<FailureT, ?>>();
         for (var toBeValidated : validatables) {
             if (toBeValidated == null) {
@@ -29,8 +33,8 @@ public class Strategies {
             } else {
                 Either<FailureT, ValidatableT> toBeValidatedRight = Either.right(toBeValidated);
                 Either<FailureT, ?> validationResult = null;
-                for (var validation : validations) {
-                    validationResult = validation.apply(toBeValidatedRight);
+                for (var currentValidation : validations) {
+                    validationResult = fireValidation(currentValidation, toBeValidatedRight, throwableMapper);
                     if (validationResult.isLeft()) {
                         break;
                     }
@@ -50,32 +54,46 @@ public class Strategies {
      * ∙ Universal Vocab across languages
      */
     public static <FailureT, ValidatableT> FailFastStrategy<ValidatableT, FailureT> failFastStrategy(
-            List<Validator<ValidatableT, FailureT>> validations, FailureT invalidValidatable) {
+            List<Validator<ValidatableT, FailureT>> validations, 
+            FailureT invalidValidatable, 
+            Function1<Throwable, FailureT> throwableMapper) {
         return toBeValidated -> {
             if (toBeValidated == null) {
                 return Either.left(invalidValidatable);
             } else {
                 Either<FailureT, ValidatableT> toBeValidatedRight = Either.right(toBeValidated);
                 final BiFunction<Either<FailureT, ?>, Validator<ValidatableT, FailureT>, Either<FailureT, ?>> applyValidation
-                        = (validated, currentValidation) -> validated.isRight() ? currentValidation.apply(toBeValidatedRight) : validated;
+                        = (validated, currentValidation) -> validated.isRight() ? fireValidation(currentValidation, toBeValidatedRight, throwableMapper) : validated;
                 return validations.foldLeft(toBeValidatedRight, applyValidation);
             }
         };
     }
+    
+    private static <FailureT, ValidatableT> Either<FailureT, ?> fireValidation(
+            Validator<ValidatableT, FailureT> validation,
+            Either<FailureT, ValidatableT> validatable,
+            Function1<Throwable, FailureT> throwableMapper) {
+        return Try.of(() -> validation.apply(validatable))
+                .fold(throwable -> Either.left(throwableMapper.apply(throwable)), identity());
+    }
 
     public static <FailureT, ValidatableT> FailFastStrategy<ValidatableT, FailureT> failFastStrategy2(
-            List<Validator<ValidatableT, FailureT>> validations, FailureT invalidValidatable) {
+            List<Validator<ValidatableT, FailureT>> validations, 
+            FailureT invalidValidatable, 
+            Function1<Throwable, FailureT> throwableMapper) {
         return toBeValidated -> toBeValidated == null
                 ? Either.left(invalidValidatable)
-                : applyValidations(toBeValidated, validations).getOrElse(Either.right(toBeValidated));
+                : applyValidations(toBeValidated, validations, throwableMapper).getOrElse(Either.right(toBeValidated));
     }
 
     private static <FailureT, ValidatableT> Iterator<Either<FailureT, ?>> applyValidations(
-            ValidatableT toBeValidated, List<Validator<ValidatableT, FailureT>> validations) {
+            ValidatableT toBeValidated, 
+            List<Validator<ValidatableT, FailureT>> validations, 
+            Function1<Throwable, FailureT> throwableMapper) {
         Either<FailureT, ValidatableT> toBeValidatedRight = Either.right(toBeValidated);
         final Iterator<Either<FailureT, ?>> validationResults =
                 validations.iterator() // This is vavr iterator, which is lazy by default.
-                        .map(validation -> validation.apply(toBeValidatedRight));
+                        .map(currentValidation -> fireValidation(currentValidation, toBeValidatedRight, throwableMapper));
         // This is just returning the description, nothing shall be run without terminal operator.
         return validationResults.filter(Either::isLeft); 
     }
@@ -83,7 +101,9 @@ public class Strategies {
     /* ---------------------------ACCUMULATION--------------------------- */
 
     public static <FailureT, ValidatableT> AccumulationStrategy<ValidatableT, FailureT> accumulationStrategy(
-            List<Validator<ValidatableT, FailureT>> validations, FailureT invalidValidatable) {
+            List<Validator<ValidatableT, FailureT>> validations, 
+            FailureT invalidValidatable,
+            Function1<Throwable, FailureT> throwableMapper) {
         return toBeValidated -> {
             if (toBeValidated == null) {
                 return List.of(Either.left(invalidValidatable));
@@ -91,7 +111,7 @@ public class Strategies {
                 Either<FailureT, ValidatableT> toBeValidatedRight = Either.right(toBeValidated);
                 return validations.foldLeft(List.<Either<FailureT, ?>>empty(),
                         (failureResults, currentValidation) -> {
-                            val result = currentValidation.apply(toBeValidatedRight);
+                            val result = fireValidation(currentValidation, toBeValidatedRight, throwableMapper);
                             return result.isLeft() ? failureResults.append(result) : failureResults;
                         });
             }
@@ -99,10 +119,20 @@ public class Strategies {
     }
 
     private <FailureT, ValidatableT> AccumulationStrategy<ValidatableT, FailureT> accumulationStrategy2(
-            List<Validator<ValidatableT, FailureT>> validations, FailureT invalidValidatable) {
+            List<Validator<ValidatableT, FailureT>> validations, 
+            FailureT invalidValidatable, 
+            Function1<Throwable, FailureT> throwableMapper) {
         return toBeValidated -> toBeValidated == null
                 ? List.of(Either.left(invalidValidatable))
-                : applyValidations(toBeValidated, validations).toList();
+                : applyValidations(toBeValidated, validations, throwableMapper).toList();
     }
 
+}
+
+@FunctionalInterface
+interface AccumulationStrategy<ValidatableT, FailureT> extends Function1<ValidatableT, List<Either<FailureT, ?>>> {
+}
+
+@FunctionalInterface
+interface FailFastStrategy<ValidatableT, FailureT> extends Function1<ValidatableT, Either<FailureT, ?>> {
 }
